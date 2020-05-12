b59cad84781d09fe208c82210f55b105e38f14dc
DeniCR <a75655@alunos.uminho.pt>
2020-05-12 14:01:12 +0100

    change url in the managmentServer

--- src/managmentServer.py
+++ src/managmentServer.py
@@ -1,33 +1,36 @@
+#!/usr/bin/env python3
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import requests
+import json
 
-url = "http://localhost:1026/v2/subscriptions/"
+url = "http://orion:1026/v2/subscriptions/"
 payload = {
 	"description": "Notify Server of all RFID context changes",
 	"subject": { 
 		"entities": [
-	    	{ "idPattern": ".*" , "type": "RFIDSensor"} 
-	    	] 
+	    	{ "idPattern": ".*"} 
+	    	]
 	},
 	"notification": {
 		"http": {
-			"url": "http://localhost:8080" }
+			"url": "http://managment:40000" 
+        }
 	},
 	"throttling": 5
 }
 headers = {
-  'Content-Type': 'application/json',
-  'fiware-service': 'openiot',
-  'fiware-servicepath': '/',
-  'Content-Type': 'text/plain'
+  'Content-Type': 'application/json'
 }
+
+lastrequest=""
 
 def post(url,headers,payload):
 	try:
 		response = requests.request("POST", url, headers=headers, data = payload)
 		#response.raise_for_status()
-		print(response.text.encode('utf8'))
+		print(response.text.encode('utf8'), flush=True)
 	except requests.exceptions.HTTPError as errh:
 			print ("Http Error:",errh)
 	except requests.exceptions.ConnectionError as errc:
@@ -44,8 +47,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         #self.wfile.write(b'Hello, world!')
+        self.wfile.write(lastrequest.encode('utf8'))
 
     def do_POST(self):
+        print("Notification recived!!!", flush=True)
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
@@ -55,13 +60,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'This is POST request. ')
         response.write(b'Received: ')
         response.write(body)
+        print(body, flush=True)
+        lastrequest=body
         self.wfile.write(response.getvalue())
-
-        #if()
 
 def main():
 	post(url,headers,json.dumps(payload))
-	httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
+	print("Start server ...", flush=True)
+	httpd = HTTPServer(("", 40000), SimpleHTTPRequestHandler)
 	httpd.serve_forever()
 
 if __name__ == "__main__":
