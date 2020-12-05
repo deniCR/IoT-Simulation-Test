fdb12adf2ff7f58ca4788bd8046ce963c1051281
Daniel <31859571+deniCR@users.noreply.github.com>
2020-12-05 10:17:57 +0000

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,6 @@
 
 date
 
-#Depending on the configuration of the docker, the use of authorizations (sudo) may be unnecessary
 sudo service grafana-server start
 sudo service mongodb stop
 sudo sysctl -w vm.max_map_count=262144
